99604ac7a375815b3bf8ec9601cd273b1e9fb7f2
Daniel Scally <44758401+dascally@users.noreply.github.com>
2025-09-25 14:52:21 -0400

    test: Add PersonRepository#GetByIdAsync tests

--- GoTournamentPlanner.Tests/PersonRepositoryTests.cs
+++ GoTournamentPlanner.Tests/PersonRepositoryTests.cs
@@ -26,6 +26,37 @@ public class PersonRepositoryTests : TestBase
     }
 
     [Fact]
+    public async Task GetByIdAsync_GivenValidId_ShouldReturnPlayer()
+    {
+        // Arrange
+        await SeedDataAsync();
+        Person existingPlayer = (await _repository.GetAllAsync()).First();
+
+        // Act
+        var result = await _repository.GetByIdAsync(existingPlayer.Id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(existingPlayer.Id, result.Id);
+        Assert.Equal(existingPlayer.Name.Family, result.Name.Family);
+        Assert.Equal(existingPlayer.Name.Given, result.Name.Given);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_GivenInvalidId_ShouldReturnNull()
+    {
+        // Arrange
+        await SeedDataAsync();
+        int invalidId = 999;
+
+        // Act
+        var result = await _repository.GetByIdAsync(invalidId);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
     public async Task RegisterPlayerAsync_ShouldCreatePerson()
     {
         // Arrange
