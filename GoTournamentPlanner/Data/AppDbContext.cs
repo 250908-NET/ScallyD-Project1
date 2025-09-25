fdcb7f1772ec0efdc47e4e2b4e7cd8947954fa3c
Daniel Scally <44758401+dascally@users.noreply.github.com>
2025-09-24 22:24:48 -0400

    feat: Configure DB context for migration

--- GoTournamentPlanner/Data/AppDbContext.cs
+++ GoTournamentPlanner/Data/AppDbContext.cs
@@ -3,4 +3,21 @@ using Microsoft.EntityFrameworkCore;
 class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Tournament> Tournaments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>()
+            .HasMany(p => p.ParticipatingIn)
+            .WithMany(t => t.Participants)
+            .UsingEntity("TournamentParticipants");
+
+        modelBuilder.Entity<Tournament>()
+            .HasOne(t => t.Organizer)
+            .WithMany()
+            .HasForeignKey("OrganizerId")
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
